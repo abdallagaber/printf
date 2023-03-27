a0ee8d609f7b7fcac46cd7d0fdd375d07c580063
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-26 20:12:12 -0700

    first addition

---
+++ printf.c
@@ -0,0 +1,44 @@
+#include"main.h"
+#include<stdarg>
+/**
+ * _printf - printf function
+ * format: ds sd s sd
+ * ...: sd ss sds
+ * Return: sd ss s
+ */
+int _printf(const char *format, ...)
+{
+va_list arg;
+int count = 0, i = 0;
+
+va_start(format,arg);
+while(format[i])
+{
+	if (format[i] == "%")
+	{
+		i++;
+		if (format[i] == 'c')
+		{
+			_putchar(va_arg(arg, int) + '0');
+			count++;
+		}
+		else if (format[i] == 's')
+		{
+			count += printf("%s",va_arg(arg, char*));
+		}
+		else if (format[i] == '%')
+		{
+			_putchar("%");
+			count++;
+		}
+	}
+	else 
+	{
+		_putchar(format[i]);
+		count++;
+		i++;
+	}
+}
+format[i] = '\0';
+return (count);
+}

f81720cb6e3bf238162a7bbbd30a3cf284dd3e21
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-26 21:32:36 -0700

    ffd

--- printf.c
+++ printf.c
@@ -2,7 +2,7 @@
 #include<stdarg.h>
 /**
  * _printf - printf function
- * format: ds sd s sd
+ * @format: ds sd s sd
  * ...: sd ss sds
  * Return: sd ss s
  */
@@ -11,8 +11,8 @@ int _printf(const char *format, ...)
 va_list arg;
 int count = 0, i = 0;
 
-va_start(arg,format);
-while(format[i])
+va_start(arg, format);
+while (format[i])
 {
 	if (format[i] == '%')
 	{
@@ -25,7 +25,7 @@ while(format[i])
 		}
 		else if (format[i] == 's')
 		{
-			count += printf("%s",va_arg(arg,char*));
+			count += printf("%s", va_arg(arg, char*));
 			i++;
 		}
 		else if (format[i] == '%')
@@ -34,12 +34,12 @@ while(format[i])
 			count++;
 			i++;
 		}
-		else 
+		else
 		{
 			i++;
 		}
 	}
-	else 
+	else
 	{
 		_putchar(format[i]);
 		count++;
